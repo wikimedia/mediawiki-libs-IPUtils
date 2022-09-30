e68cf6a14efe083e77723d686d5381ed9e64dc73
Reedy <reedy@wikimedia.org>
2022-09-30 01:18:07 +0100

    Minor code cleanup
    Change-Id: Ia2d9936c35c2cf9473c2b294ca6dddc738cfb76c

--- .phan/config.php
+++ .phan/config.php
@@ -1,7 +1,7 @@
 <?php
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['directory_list'][] = 'vendor/';
-$cfg['exclude_analysis_directory_list'][] = 'vendor/';
+$cfg['directory_list'][] = 'vendor/wikimedia/';
+$cfg['exclude_analysis_directory_list'][] = 'vendor/wikimedia/';
 
 return $cfg;
