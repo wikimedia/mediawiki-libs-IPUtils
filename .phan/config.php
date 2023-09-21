6100e76a7e1bed88a2417c129074abdd3a9531e7
Timo Tijhof <krinkle@fastmail.com>
2023-09-21 14:34:23 -0700

    [BREAKING CHANGE] Require PHP 7.4 or later
    In preparation for integrating wikimedia/ip-set into this library,
    match its PHP requirement. We currently got away with not bumping this
    because IPUtils has remained agnostic from ip-set 2.x to 4.x, but once
    we merge the libraries, these need to have the same requirement.
    
    Also bump mediawiki/mediawiki-phan-config while at it, because this
    repo is currently failing Phan on local development when tested on
    PHP 8.2 due to https://github.com/phan/phan/issues/4751, which was
    fixed in Phan 5.4.2, and mediawiki/mediawiki-phan-config 0.13, updates
    use of Phan from 5.4.1 to 5.4.2.
    
    Change-Id: Id2ba7d59a46068a23bd30ab2717f62c8c4da0e28

--- .phan/config.php
+++ .phan/config.php
@@ -1,3 +1,12 @@
 <?php
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
-return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
+// https://phabricator.wikimedia.org/T325321
+$cfg['plugins'] = [];
+
+$cfg['directory_list'] = [
+	'vendor/wikimedia/',
+	'src/',
+];
+
+return $cfg;
