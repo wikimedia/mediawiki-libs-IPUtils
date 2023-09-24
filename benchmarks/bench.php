a689a6605a97de2c386383c9b1ae75d701c71dea
Timo Tijhof <krinkle@fastmail.com>
2023-09-23 20:30:34 -0700

    Import IPSet class into IPUtils library
    * Commit history preserved original Gerrit Code Review URLs from
      git-notes embedded in the commit messages.
    
    * Minor tweaks in this commit to accomodate consolidation of README,
      differences in CI config, and to match latest boilerplate
      from mediawiki/tools/cookiecutter-library.git.
    
    Change-Id: I59e55b9d08d1075dce0c13fbfe8bb8b0f8fb96a8

--- benchmarks/bench.php
+++ benchmarks/bench.php
@@ -21,7 +21,7 @@ echo "Load from serialized json and initialise: {$t}s\n";
 
 checkIPs( $ipsetJSON );
 
-function checkIPs( IPSet $ipset ) {
+function checkIPs( IPSet $ipset ): void {
 	foreach ( [ '69.63.176.1' => true, '127.0.0.1' => false ] as $ip => $exists ) {
 		if ( $ipset->match( $ip ) === $exists ) {
 			echo "IPSet returned correct result for $ip\n";
