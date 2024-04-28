787bc03c594edf6127d212a8dee9a5a806396c84
Reedy <reedy@wikimedia.org>
2024-04-28 13:52:38 +0100

    Simplify phan config
    Change-Id: I680b857edeb391bf826b77e565e22ddd545ec444

--- .phan/config.php
+++ .phan/config.php
@@ -1,12 +1,2 @@
 <?php
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
-
-// https://phabricator.wikimedia.org/T325321
-$cfg['plugins'] = [];
-
-$cfg['directory_list'] = [
-	'vendor/wikimedia/',
-	'src/',
-];
-
-return $cfg;
+return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
