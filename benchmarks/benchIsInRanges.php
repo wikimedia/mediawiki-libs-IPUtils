e8e9c9afb40a59d6a2e667baa40699adcbd4c107
Ori Livneh <ori.livneh@gmail.com>
2025-11-15 17:45:08 -0500

    Add benchmark for IPUtils::isInRanges
    This is the hottest function (in total time) in the IPUtils library on
    Wikimedia's infrastructure.
    
    The set of IP ranges is derived from $wgSoftBlockRanges in
    operations/mediawiki-config@300f617414. The two IPs tested against these
    ranges are my current IPv4 and IPv6 addresses.
    
    Change-Id: I93830f2ae8894a5184cd355c281b15753e80ad1c

---
+++ benchmarks/benchIsInRanges.php
@@ -0,0 +1,29 @@
+<?php
+// Benchmark for IPUtils::isInRanges().
+
+require_once dirname( __DIR__ ) . '/src/IPUtils.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
+use Wikimedia\IPUtils;
+
+$softBlockRanges = require __DIR__ . '/softBlockRanges.php';
+
+$iterations = 1000;
+
+$startTime = hrtime( true );
+
+// Randomly-chosen IPs. To simulate the common case, they are not
+// in any of the ranges.
+$ipv6 = '2600:4041:42ad:b800:3ca0:8106:2c2d:e290';
+$ipv4 = '71.172.245.55';
+
+for ( $i = 0; $i < $iterations; $i += 2 ) {
+	IPUtils::isInRanges( $ipv4, $softBlockRanges );
+	IPUtils::isInRanges( $ipv6, $softBlockRanges );
+}
+
+$endTime = hrtime( true );
+$totalTimeMs = ( $endTime - $startTime ) / 1_000_000;
+$avgTimeMs = $totalTimeMs / $iterations;
+
+echo "Average time: " . sprintf( '%.3f', $avgTimeMs ) . " ms\n";
